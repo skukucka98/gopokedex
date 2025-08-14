3c49437d617a8c7a9178ada8296377dbc4c8809e
skukucka98 <skukucka98@gmail.com>
2025-08-13 22:23:19 -0400

    second commit

--- main.go
+++ main.go
@@ -1,10 +1,20 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
+	"strings"
 )
 
 func main() {
-	input := cleanInput("hello World")
-	fmt.Println(input)
+	scanner := bufio.NewScanner(os.Stdin)
+	for {
+		fmt.Print("Pokedex > ")
+		scanner.Scan()
+		cleanText := cleanInput(scanner.Text())
+		textList := strings.Split(cleanText[0], " ")
+		command := textList[0]
+		fmt.Println("Your command was:", command) // Println will add back the final '\n'
+	}
 }
